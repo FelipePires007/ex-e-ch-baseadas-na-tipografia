7f29f2ea3bd8da3f7ef031a6d97fe14d99c0b312
Felipe Pires <felipegamer649@gmail.com>
2025-07-26 22:48:02 -0300

    Add folder and organizing

---
+++ js/script1.js
@@ -0,0 +1,30 @@
+const fontSelect = document.getElementById('fontSelect');
+const lineHeightInput = document.getElementById('lineHeight');
+const inputWidthInput = document.getElementById('inputWidth');
+const testInput = document.getElementById('testInput');
+const lhValueLabel = document.getElementById('lhValue');
+const chValueLabel = document.getElementById('chValue');
+
+function updateStyles() {
+    const font = fontSelect.value;
+    const lineHeight = lineHeightInput.value;
+    const inputWidth = inputWidthInput.value;
+
+    testInput.style.fontFamily = font;
+    testInput.style.lineHeight = lineHeight + 'ex';
+    testInput.style.width = inputWidth + 'ch';
+    testInput.style.lineHeight = lineHeight + 'ex';
+    testInput.style.height = lineHeight + 'ex'; 
+
+
+    lhValueLabel.textContent = lineHeight;
+    chValueLabel.textContent = inputWidth;
+}
+
+fontSelect.addEventListener('change', updateStyles);
+lineHeightInput.addEventListener('input', updateStyles);
+inputWidthInput.addEventListener('input', updateStyles);
+
+updateStyles();
+
+
