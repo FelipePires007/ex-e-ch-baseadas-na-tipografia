b08a5629853626c60711a4523c674abff65394ba
Felipe Pires <felipegamer649@gmail.com>
2025-07-26 22:51:12 -0300

    Delete

--- script.js
+++
@@ -1,7 +0,0 @@
-const fontSelect = document.getElementById("fontSelect");
-const inputField = document.getElementById("inputField");
-
-fontSelect.addEventListener("change", () => {
-    const selectedFont = fontSelect.value;
-    inputField.style.fontFamily = selectedFont;
-});
